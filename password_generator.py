5a7ff4ee00e6152a9940f89726cb8f26321fb41a
Shanti Rajak <shantirajak552@gmail.com>
2026-09-03 21:36:47 +0530

    Delete password_generator.py

--- password_generator.py
+++
@@ -1,9 +0,0 @@
-import string
-import random
-characters = string.ascii_uppercase + string.ascii_lowercase + string.digits + string.punctuation
-
-length = int(input("Enter password length: "))
-password=""
-for i in range(length):
-    password = password + random.choice(characters )
-print("password generated:    ",password )    
